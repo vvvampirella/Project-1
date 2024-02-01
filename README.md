edbafc0967a417d4ed48125877712b5474f623aa
vvvampirella <anselmiella4@gmail.com>
2024-02-01 11:20:35 -0500

    project 1 bugs

--- README.md
+++ README.md
@@ -1,39 +1,21 @@
 # Project-1
 // Make and Build Your Own Game
 
-// This game is a trivia game and will have random questions for users to try to get correctly. After the player answers all of the questions the game stops and will show the user their scores.
+ // Technologies used: HTML, CSS, JavScript.
 
-// For the first page, the user will have two options to choose from. I did this because not everyone wants to play a basic trvivia game and I know as a user I would like to have certain options I can choose from.
+ // Link to live sight:
 
-//Sports Trivia Game
-
-// A Sports trivia option. This option will have questions all about sports. This option is for users who would like to test and see how much they know about 
-Sports. This option is for users who are confident that they know a lot about the world of sports.
-
-// Movie Trivia Game
-
-// A movie trivia game. This option is for users who are well versed in the world of film and who would like to see how much they know. This is most likely the option I would pick because I know a lot about movies. This option will contain questions related to movies such as: "Where did Coraline from the movie "Coraline" live?"
-
-// This game will be colorful and visually pleasing. 
-the first page will have the "movie trivia" option (button) and the "sport trivia" option (button) as well as a box showing the rules/information of the game. 
-
-// The layout of the actual trivia game part will be a question and then a submit box where the user can type in their answers and submit them. After they submit their answer they will get a "you are correct" or "you are wrong" and then another question will appear.
-
-// Reset rules: there will be an option at the end of every game to reset and try again.
-
-// Stats: at the end of every game there will be how many questions you got right or wrong.
+// Installation Instructions: 
+Through GitHub repo or my link to my live sightt.
 
 
-// This will be a fun game to test players on their knowledge of their favorite niches!
+// Approach Taken: 
+With this game, I made a sports trivia game because I know that it is a very popular trivia to play and a lot of people will use this genre of trivia game. So if i were to release this it would be more popular than more of a "niche" genre. I wanted this to be a fun game for people who are interested in sports to play and test their knowledge.
 
 
-// link to my live sight https://prod.liveshare.vsengsaas.visualstudio.com/join?682B2A6B9A38AF1FC2E43530455C45904298
+// User Stories:
+I want the users tto be able to hop on a colorful vibrant game with questions about their favorite topic, sports! This will be where they can test their knwowledge and get the highest score! I made a colorful background and engaging questions. At the end of the game there are their scores that show how many they got right or wrong along with a reset button that will allow them to reset their game and try to beat their previous score. The questions will be randomized so they cant just copy down the questions they already answered. There will also be a "you got it!" or "Try again!" when the user answers the question.
 
 
-// there is a bug i am missing something at the last minute and i cant figure it out so tomorrow i will ask. but css and html are done. also i had to simplify it because i underestimated the difficulty of my original plan.
-
-
-
-
-
-
+// BUGS:
+there is a problem with my game to where when you answer it wont go to the next question.
--- script.js
+++ script.js
@@ -1,8 +1,3 @@
-// theres a bug i cant figure out at the last minute so im waiting until tomorrow 
-// to fix it with the instructors because im pretty sure i am only missing one thing
-
-
-
 let questions = [
   {question: 'What’s the diameter of a basketball hoop in inches?',
     answer: '18 inches'},
@@ -15,12 +10,101 @@ answer: 'lacrosse'},
 {question: 'How many dimples does an average golf ball have?',
 answer: '336'},
 {question: 'What country has competed the most times in the Summer Olympics yet hasn’t won a gold medal?', 
-answer: 'the philippines'}
-
+answer: 'the philippines'},
+{question: 'In motor racing, what color is the flag they wave to indicate the winner?',
+answer: 'Checkered flag'},
+{question: 'How many holes are played in an average round of golf?',
+answer: '18'},
+{question: 'What color are the goalposts in football?',
+answer: 'Yellow'},
+{question: 'How long is a marathon?',
+answer: '26.2 miles'},
+{question: 'In what game is “love” a score?',
+answer: 'Tennis'},
+{question: 'What sport is a lot like softball?',
+answer: 'Baseball'},
+{question: 'In meters, how big is an Olympic-sized swimming pool?',
+answer: '50 meters long and 25 meters wide'},
+{question: 'In football, how many points does a touchdown hold?',
+answer: '6 points'},
+{question: 'How many players are on a baseball team?',
+answer: '9 players'},
+{question: ' In soccer, what body part can’t touch the ball?',
+answer: 'Hands'},
+{question: 'What sporting equipment is used for striking a tennis ball?',
+ answer: 'Tennis racquet'},
+{question: 'Which sport uses a net, a racket, and a shuttlecock?',
+answer: 'Badminton'},
+{question: 'Which of the following sports does not use a ball? Golf, tennis, hockey, or polo?',
+answer: 'Hockey'},
+{question: 'What type of race is the Tour de France?',
+answer: 'Bicycle race'},
+{question: 'How many sports were included in the 2008 Summer Olympics?',
+answer: '28'},
+{question: 'A sporting event is held every year on Memorial Day. What is it?',
+answer: 'Indianapolis 500'},
+{question: 'What number sets to the right of the number ‘20’ on a standard dartboard?',
+answer: '1'},
+{question: 'In what year were women allowed to compete in the modern Olympic games and in what sport?',
+answer: '1900, tennis'},
+{question: 'Basketball player, Scottie Pippen, has a word tattooed on his forearm. What does it say?',
+answer: 'Pip'},
+{question: 'What sport is Bela Karolyi known for coaching?',
+answer: 'Women’s gymnastics'},
+{question: 'What is the only sport to be played on the moon?',
+answer: 'A round of golf'},
+{question: 'How old was Tiger Woods when he won the Masters?',
+answer: '21'},
+{question: 'In which winter sport are the terms “stale fish” and “mule kick” used?',
+answer: 'Snowboarding'},
+{question: 'How long is the free skate in figure skating?',
+answer: 'Four and a half minutes for men, four minutes for women'},
+{question: 'Who was the first president to throw the ceremonial first pitch at a Major League Baseball game?',
+answer: 'William Howard Taft'},
+{question: 'How many minutes was the longest recorded point in the history of tennis?',
+answer: '29 minutes'},
+{question: 'During the first-ever modern Olympics, what were the first placers awarded with?',
+answer: 'Silver medals'},
+{question: 'How many Olympic games were held in countries that no longer exist?',
+answer: '3'},
+{question: 'What NFL team was originally called the ‘New York Titans’?',
+answer: 'The New York Jets'},
+{question: 'What is the only team in the NFL to neither host nor play in the Super Bowl?',
+answer: 'Cleveland Browns'},
+{question: 'How much does an NFL football weigh?',
+answer: '1 pound'},
+{question: 'This player was the first-ever openly gay person to be drafted by an NFL team. What was his name?',
+answer: 'Michael Sam'},
+{question: 'What NFL team lost the first Super Bowl in 1970',
+answer: 'Minnesota Vikings'},
+{question: 'What team is considered the oldest in the NFL?',
+answer: 'Green Bay Packers'},
+{question: 'What team won 3 Super Bowls in the 1990s?',
+answer: 'The Dallas Cowboys'},
+{question: 'What team won the very first NBA game in 1946?',
+answer: 'The New York Knicks'},
+{question: 'Who was the first WNBA player to dunk in a playoff game?',
+answer: 'Brittney Griner'},
+{question: 'Who was the first NBA player to test positive for COVID-19?',
+answer: 'Rudy Gobert'},
+{question: 'Who was the first NBA player to shatter a backboard?',
+answer: 'Chuck Connors'},
+{question: 'What material was first used to cover baseballs?',
+answer: 'Cowhide'},
+{question: 'What team gets the advantage of the first bat?',
+answer: 'The visiting team'},
+{question: 'What female pitcher once struck out Babe Ruth and Lou Gehrig?',
+answer: 'Jackie Mitchell'},
+{question: 'What is the most stolen base?',
+answer: 'Second base'},
+{question: 'What are the Minnesota Twins named after?',
+answer: 'The Twin Cities'},
+{question: ' What is a soccer field called?',
+answer: 'A pitch'}
 ]
 
-
 let questionDom = document.querySelector(".questions")
+
 
 
 // selects a question
@@ -28,32 +112,65 @@ function getRandomQuestion(arr){
   return arr[Math.floor(Math.random() * arr.length)]
 }
 
-// 
-let randomQuestionsPair = getRandomQuestions(questions);
-console.log('random question', randomQuestionsPair.questions)
-console.log('random answer', randomQuestionsPair.answer)
+// random queston and answer pairs
+let randomQuestionsPair = getRandomQuestion(questions);
+console.log('random question', randomQuestionsPair.question);
+console.log('random answer', randomQuestionsPair.answer);
+
+// questionDom.innerHTML = `<h1>${randomQuestionsPair.question}</h1>`
 
 
 let randomQuestionsDom = function(){
-  return questionsDom.innerHTML = `<h1>${questionsPair.questions}</h1>`
+  // generating question in DOM: web page
+  return questionDom.innerHTML = `<h1>${randomQuestionsPair.question}</h1>`
 }
-// randomQuestionDom(); // BUG
+ randomQuestionsDom(); 
 
 
-let userAnswer = document.querySelector('#answers');
-document.querySelector('.sub').addEventListener('click', function(){
 
-  if (userAnswer.value.toLowerCase() == questionsPair.answer) {
-    console.log('you got it')
-    userAnswer.value = '';
-    document.querySelector('.winner').textContent = 'you are right';
+// submit button that will either show right or wrong
+const gameAnswer = document.querySelector('#answers');
+
+const submit = document.querySelector('.sub');
+submit.addEventListener('click', function() {
+
+  console.log(`///////////////////`)
+  randomQuestionsDom() // BUG this needs to generate a new question after the submit button is clicked
+
+  // shows if the player is right
+ if (gameAnswer.value.toLowerCase() == randomQuestionsPair.answer) {
   
-  } else {
 
-    console.log(`try again`)
-    document.querySelector('.winner').textContent = 'you are wrong'
-  
-  }
+  document.querySelector('.winner').textContent = `Yay you are right!!`;
+
+// else if the player is wrong
+ } else {
+  console.log('try again')
+  document.querySelector('.winner').textContent = `try again`;
+
+
+ }
+
+
+
+
 })
 
+// submit.addEventListener('click', randomQuestionsDom)
+
+// let userAnswer = document.querySelector('#answers');
+// document.querySelector('.sub').addEventListener('click', function(){
+
+//   if (userAnswer.value.toLowerCase() == questionsPair.answer) {
+//     console.log('You got it!')
+//     userAnswer.value = '';
+//     document.querySelector('.winner').textContent = 'You are right!';
+  
+//   } else {
+
+//     console.log(`Try again`)
+//     document.querySelector('.winner').textContent = 'You are wrong'
+  
+//   }
+// })
 
